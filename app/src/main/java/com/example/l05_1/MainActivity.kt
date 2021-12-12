91f623e35b8c1a0633612826fabfedc5863dd7fc
ksproska <kamila.sproska@gmail.com>
2021-12-12 20:55:53 +0100

    icons 2

--- app/src/main/java/com/example/l05_1/MainActivity.kt
+++ app/src/main/java/com/example/l05_1/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.l05_1
 
+import android.content.ClipData
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.TextView
+import androidx.core.view.get
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
@@ -17,10 +21,16 @@ class MainActivity : AppCompatActivity() {
         navView.setNavigationItemSelectedListener { menuItem ->
             when(menuItem.itemId) {
 //                R.id.drawer_button1 -> startActivity(Intent(this, MainActivity::class.java))
-                R.id.drawer_button2 -> startActivity(Intent(this, Activity2::class.java))
-                R.id.drawer_button3 -> startActivity(Intent(this, Activity3::class.java))
+                R.id.drawer_button2 -> {
+                    drawerLayout.close()
+                    startActivity(Intent(this, Activity2::class.java))
+                }
+                R.id.drawer_button3 -> {
+                    drawerLayout.close()
+                    startActivity(Intent(this, Activity3::class.java))
+                }
             }
-//            drawerLayout.close()
+
             true
         }
     }
