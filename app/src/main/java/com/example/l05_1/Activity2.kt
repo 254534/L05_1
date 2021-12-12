b21e01ec50180e067f6bc486609843fd57f8ce66
ksproska <kamila.sproska@gmail.com>
2021-12-12 14:15:11 +0100

    options menu submenu works

--- app/src/main/java/com/example/l05_1/Activity2.kt
+++ app/src/main/java/com/example/l05_1/Activity2.kt
@@ -2,6 +2,8 @@ package com.example.l05_1
 
 import android.content.Intent
 import android.graphics.Color
+import android.graphics.Typeface
+import android.graphics.fonts.Font
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -31,22 +33,37 @@ class Activity2 : AppCompatActivity() {
         val text2: EditText = findViewById(R.id.editText2)
         text1.setBackgroundColor(color)
         text2.setBackgroundColor(color)
+        text1.setTextColor(Color.WHITE)
+        text2.setTextColor(Color.WHITE)
+    }
+
+    fun changeFontBothTexts(typeface: Typeface) {
+        val text1: EditText = findViewById(R.id.editText1)
+        val text2: EditText = findViewById(R.id.editText2)
+        text1.typeface = typeface
+        text2.typeface = typeface
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-
-
         return when(item.itemId) {
             R.id.activity2_option1 -> {
-                changeBackgroundColorBothTexts(Color.RED)
+                changeBackgroundColorBothTexts(Color.rgb(216,9,126))
                 true
             }
             R.id.activity2_option2 -> {
-                changeBackgroundColorBothTexts(Color.GREEN)
+                changeBackgroundColorBothTexts(Color.rgb(140,87,156))
                 true
             }
             R.id.activity2_option3 -> {
-                changeBackgroundColorBothTexts(Color.BLUE)
+                changeBackgroundColorBothTexts(Color.rgb(36,70,142))
+                true
+            }
+            R.id.activity2_submenu1_option1 -> {
+                changeFontBothTexts(Typeface.MONOSPACE)
+                true
+            }
+            R.id.activity2_submenu1_option2 -> {
+                changeFontBothTexts(Typeface.DEFAULT_BOLD)
                 true
             }
 
