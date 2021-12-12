39da0535b45da05297ac6f703491b89e2d08dc52
ksproska <kamila.sproska@gmail.com>
2021-12-12 18:51:06 +0100

    floating button works

--- app/src/main/java/com/example/l05_1/MainActivity.kt
+++ app/src/main/java/com/example/l05_1/MainActivity.kt
@@ -3,10 +3,13 @@ package com.example.l05_1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    private var counter = 0
     fun setNavigationButtons() {
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
@@ -17,7 +20,7 @@ class MainActivity : AppCompatActivity() {
                 R.id.drawer_button2 -> startActivity(Intent(this, Activity2::class.java))
                 R.id.drawer_button3 -> startActivity(Intent(this, Activity3::class.java))
             }
-
+//            drawerLayout.close()
             true
         }
     }
@@ -26,5 +29,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setNavigationButtons()
+
+        val fab = findViewById<FloatingActionButton>(R.id.floatingActionButton)
+        fab.setOnClickListener { view ->
+            counter += 1
+            findViewById<TextView>(R.id.textViewCounter).text = counter.toString()
+        }
     }
 }
