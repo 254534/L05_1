0dad1a357699e3a1d57acb2be221263b2a9562fb
ksproska <kamila.sproska@gmail.com>
2021-12-12 12:25:05 +0100

    nav_graph.xml created

--- app/src/main/java/com/example/l05_1/MainActivity.kt
+++ app/src/main/java/com/example/l05_1/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.l05_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
     }
 }
